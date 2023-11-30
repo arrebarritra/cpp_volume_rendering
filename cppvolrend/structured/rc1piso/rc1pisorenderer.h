4eda5a0fe4069be9e3aec004cbd6715acd3db830
Aritra Bhakat <arre2barre@gmail.com>
2023-11-30 10:37:20 +0100

    Add Isosurface raytracer

---
+++ cppvolrend/structured/rc1piso/rc1pisorenderer.h
@@ -0,0 +1,42 @@
+/**
+ * Isosurface raycaster.
+ *
+ * @author Tino Weinkauf
+**/
+#pragma once
+
+#include "../../volrenderbase.h"
+
+class RayCasting1PassIso : public BaseVolumeRenderer
+{
+public:
+  RayCasting1PassIso();
+  virtual ~RayCasting1PassIso();
+  
+  virtual const char* GetName();
+  virtual const char* GetAbbreviationName();
+  virtual vis::GRID_VOLUME_DATA_TYPE GetDataTypeSupport();
+
+  virtual void Clean();
+  virtual bool Init(int shader_width, int shader_height);
+  virtual void ReloadShaders();
+
+  virtual bool Update(vis::Camera* camera);
+  virtual void Redraw();
+
+  virtual void FillParameterSpace(ParameterSpace& pspace) override;
+
+  virtual void SetImGuiComponents();
+
+protected:
+  float m_u_isovalue;
+  float m_u_step_size; // Quality of isosurface detection
+
+  glm::vec4 m_u_color; // RGBA, float
+  bool m_apply_gradient_shading;
+
+private:
+  gl::ComputeShader* cp_shader_rendering;
+
+};
+
